9d87f09339f6be1fc3d153b67c0894015cc6fc25
Rudy Sanchez <rudys212@gmail.com>
2021-09-15 02:46:03 -0700

    created te current weather card with icon and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var searchResults = document.querySelector("#searchHistory");
 var searchButton = document.querySelector("#searchBtn");
 var searchForm = document.querySelector("#searchForm");
 var fiveDayForecast = document.querySelector("#weatherForecast");
+var iconPic = document.querySelector("#icon");
+var fiveIconPic = document.querySelector("#fiveIcon");
 
 var searchHistory = [];
 
@@ -50,8 +52,9 @@ function searchedWeather(city) {
 
 function pastSearch(event) {
   var currentCity = event.target.getAttribute("historyCities");
-  showWeather(currentCity);
-  fiveDayWeather(currentCity);
+  if (currentCity) {
+    showWeather(currentCity);
+  }
 }
 
 // search history buttons
@@ -62,7 +65,7 @@ var latestSearches = function (latestSearches) {
   historySearchBtn.setAttribute("type", "button");
   historySearchBtn.setAttribute("class", "btn btn-outline-success");
 
-  historySearch.prepend(historySearchBtn);
+  historySearch.appendChild(historySearchBtn);
 };
 
 // current weather display
@@ -70,23 +73,28 @@ var showWeather = function (weather, citySearch) {
   currentSec.textContent = "";
   currentResults.textContent = citySearch;
 
-  var dayname = document.createElement("p");
-  Date(dayname.dt * 1000).toLocaleDateString("en-US");
-  dayname.appendChild.currentSec;
+  var dayname = new Date(weather.dt * 1000);
+  var day = document.createElement("p");
+  day.textContent = dayname.toLocaleDateString("en-US");
+
+  var iconData = weather.weather[0].icon;
+  var icon = "https://openweathermap.org/img/wn/" + iconData + "@2x.png";
+  iconPic.setAttribute("src", icon);
 
   var humidity = document.createElement("p");
-  humidity.textContent = "Humidty: " + weather.main.humidity + "%";
+  humidity.textContent = "Humidity: " + weather.main.humidity + "%";
 
   var windSpeed = document.createElement("p");
   windSpeed.textContent = "Wind speed:" + weather.wind.speed + "mph";
 
   var temperature = document.createElement("p");
-  temperature.classList = "list-group-item";
+  temperature.textContent = "Current Temperature: " + weather.main.temp + "°F";
 
   currentSec.appendChild(temperature);
   currentSec.appendChild(humidity);
+
   currentSec.appendChild(windSpeed);
-  currentSec.appendChild(dayname);
+  currentResults.appendChild(day);
 };
 
 fiveDayWeather = function (city) {
@@ -96,15 +104,20 @@ fiveDayWeather = function (city) {
 
   fetch(forecastReq).then(function (response) {
     response.json().then(function (data) {
-      for (var i = 0; i < data.list.length; i = i + 10) {
+      for (var i = 0; i < data.list.length; i += 8) {
         var fiveDays = document.createElement("div");
         fiveDays.textContent = "Five Day Forecast";
         fiveDays.textContent.classList = "list-group-item";
 
+        var iconInfo = data.list[i].weather[0].icon;
+        var icon = "https://openweathermap.org/img/wn/" + iconInfo + "@2x.png";
+        fiveIconPic.setAttribute("src", icon);
+
+        var dayname = new Date(data.list[i].dt * 1000);
         var dates = document.createElement("p");
-        dates.textContent = data.list[i].dt_txt;
-        dates.classList = "list-group-item";
-        fiveDayForecast.append(date);
+        dates.textContent = dayname.toLocaleDateString("en-US");
+
+        fiveDayForecast.append(dates);
 
         var temperature = document.createElement("p");
         temperature.textContent = "Temperature" + data.list[i].main.temp + "°f";
@@ -112,7 +125,7 @@ fiveDayWeather = function (city) {
         fiveDayForecast.append(temperature);
 
         var humidity = document.createElement("p");
-        humidity.textContent = "Humidty: " + data.list[i].main.humidity;
+        humidity.textContent = "Humidity: " + data.list[i].main.humidity;
         humidity.classList = "list-group-item";
         fiveDayForecast.append(humidity);
 
@@ -127,10 +140,11 @@ fiveDayWeather = function (city) {
 };
 
 displayFiveDayWeather = function (displayFiveDayWeather) {
-  var days = document.createElement("div");
+  var dates = document.createElement("div");
   fiveDayForecast.textContent = "";
   fiveDayForecast.textContent.classList = displayFiveDayWeather;
 };
 
 searchButton.addEventListener("click", currentWeatherSearch);
-currentCitySearch.addEventListener("click", showWeather);
+// currentCitySearch.addEventListener("click", showWeather);
+historySearch.addEventListener("click", pastSearch);
